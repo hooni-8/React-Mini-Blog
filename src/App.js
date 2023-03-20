36b61336076a01e5b319932199a0b2b072cc6011
Lee_SH <lee990726@gmail.com>
2023-03-21 01:32:04 +0900

    insert

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import styled from "styled-components";
+
+//pages
+import MainPage from "./component/page/MainPage";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+
+// import './App.css';
+
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+function App(props) {
+  return (
+      <BrowserRouter>
+        <MainTitleText>상훈의 미니 블로그</MainTitleText>
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="post-write" element={<PostWritePage />} />
+          <Route path="post/:postId" element={<PostViewPage />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App;
